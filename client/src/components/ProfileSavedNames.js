dda4f95f783b0ee8ebb71c4c8a95b983c7c7dbfe
sumtwelve <sumtwelve@gmail.com>
2023-05-23 23:57:28 -0600

    more boilerplate

--- client/src/components/ProfileSavedNames.js
+++ client/src/components/ProfileSavedNames.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import "../css/profile.css";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from 'react-router-dom';
+
+import { useQuery, useMutation } from '@apollo/client';
+import { ADD_FAVORITE, REMOVE_FAVORITE } from '../utils/mutations';
+import { QUERY_ONE_USER, QUERY_ALL_USERS } from "../utils/queries";
+
+import Auth from '../utils/auth';
 
 const ProfileSavedNames = () => {
 

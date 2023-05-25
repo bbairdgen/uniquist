e5a64283684b6fb2e1ac025b95728d748cb55dfc
jerk-store <taran.leu@outlook.com>
2023-05-25 16:38:25 -0600

    adding footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink
+  createHttpLink,
 } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -16,36 +16,37 @@ import SignUp from "./pages/SignUp";
 import AllUsers from "./pages/AllUsers";
 import Profile from "./pages/Profile";
 import Band from "./pages/BandPage";
-import CreateBand from './pages/CreateBand';
+import CreateBand from "./pages/CreateBand";
+import Footer from "./components/Footer";
 
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 import { Token } from "graphql";
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
-    console.log('graphQLErrors', graphQLErrors);
+    console.log("graphQLErrors", graphQLErrors);
   }
   if (networkError) {
-    console.log('networkError', networkError);
+    console.log("networkError", networkError);
   }
 });
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -54,7 +55,7 @@ const link = ApolloLink.from([errorLink, httpLink]);
 
 const client = new ApolloClient({
   link: authLink.concat(link),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 function App() {
@@ -73,12 +74,16 @@ function App() {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/allusers" element={<AllUsers />} />
             <Route path="/profile/:profileID" element={<Profile />} />
-            <Route path="/profile/:profileID/new-band" element={<CreateBand />} />
+            <Route
+              path="/profile/:profileID/new-band"
+              element={<CreateBand />}
+            />
             <Route path="/bands/:bandID" element={<Band />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </Router>
+      <Footer />
     </ApolloProvider>
   );
 }

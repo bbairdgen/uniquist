1df21fc9484a592c0d103c21e2e7b5b89e1c19a1
sumtwelve <sumtwelve@gmail.com>
2023-05-25 14:50:23 -0600

    fix server router overriding react router

--- server/server.js
+++ server/server.js
@@ -21,7 +21,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+// Allow server to render all routes indiscriminately
+// so that React Router can handle the actual routing.
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 

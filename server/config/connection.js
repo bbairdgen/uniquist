a85617e0d43f7dd996cdbb3717530c2f2f8f25d9
bbairdgen <bbairdgen@gmail.com>
2023-05-24 20:58:46 -0600

    hiding api keys

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,3 +1,4 @@
+import "dotenv/config"
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI);
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+import "dotenv/config"
 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');

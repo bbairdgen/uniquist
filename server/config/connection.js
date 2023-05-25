9c5869c0d6204eeae3ec9fbc7be5af5f027e0492
bbairdgen <bbairdgen@gmail.com>
2023-05-24 20:04:34 -0600

    deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/uniquistDB');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;

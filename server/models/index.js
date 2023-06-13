ff0b6c56449a6725a5110d8f5f02f765326f4c4c
sumtwelve <sumtwelve@gmail.com>
2023-06-13 14:39:31 -0600

    update model index

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ const Band = require("./Band");
 const Post = require("./Post");
 const Reaction = require("./Reaction");
 const Comment = require("./Comment");
+const Media = require("./Media")
 
-module.exports = { User, Band, Post, Reaction, Comment };
+module.exports = { User, Band, Post, Reaction, Comment, Media };
